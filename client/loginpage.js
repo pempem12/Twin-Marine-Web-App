018b8be857d2d049d1d9ccdebf5460cb25dfb146
Dylan Ferris <myaka27too@gmail.com>
2020-11-05 17:10:01 -0500

    Fixed script/stylesheet routing

--- client/loginpage.js
+++
@@ -1,7 +0,0 @@
-const tryLogin = () => {
-    const email = document.getElementById("email-field").value;
-    const password = document.getElementById("password-field").value;
-
-    console.log(`This is the email: ${email}`);
-    console.log(`This is the password: ${password}`);
-};
