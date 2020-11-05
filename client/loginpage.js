8bcba31cab8d173916887df79600d263160c5193
Bandy <pemisin.toyo@gmail.com>
2020-11-05 22:56:04 +0100

    Here lies the login page code

---
+++ client/loginpage.js
@@ -0,0 +1,7 @@
+const tryLogin = () => {
+    const email = document.getElementById("email-field").value;
+    const password = document.getElementById("password-field").value;
+
+    console.log(`This is the email: ${email}`);
+    console.log(`This is the password: ${password}`);
+};
