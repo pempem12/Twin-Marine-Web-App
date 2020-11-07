89527bf83699ea683fc7c99706406569a288a189
nikewall <questionsforninjaguyroblox@gmail.com>
2020-11-06 23:23:32 -0500

    Destroy session on logout. Added status codes to log user state in /* endpoint temporarily.

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -10,8 +10,32 @@ export const route = (app, endpoint) => {
     app.post(endpoint, auth.createParentUser);
     app.post(`${endpoint}/login`, auth.login);
 
-    // All following routes require the user to be logged in
-    app.use(`${endpoint}/*`, auth.authenticateSession(false));
+    app.post(`${endpoint}/logout`, (req, res) => {
+        // If we are logged in, destroy the session (logout) successfully (200).
+        if (req.session.user) {
+            req.session.destroy(function (err) {
+                if (err) throw err;
 
-    app.post(`${endpoint}/logout`, (req, res) => res.status(200).send("Logout successful!\n"));
+                res.status(200).send('Logged out');
+            });
+        // If NOT logged in, just return 401.
+        } else {
+            res.status(401).send('Unauthorized');
+        }
+   });
+
+    // All following routes require the user to be logged in
+    app.use(`${endpoint}/*`, (req, res) => {
+        auth.authenticateSession(false);
+
+        // TODO: I'm temp code! Remove me!
+        // If user is logged in, show 200.
+        if (req.session.user){
+            res.status(200).send(`Logged in`);
+        // If not logged in, show 401.
+        } else {
+            res.status(401).send(`Not logged in`);
+        }
+    });
+ 
 }
